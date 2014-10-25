8674c7704a45001132bda0ad12ecfbeaf7132fab
ricrpi <rjhender@msn.com>
2014-10-25 20:15:18 +0100

    fixed gcc includes

--- r4300-code-analysis/Debug/InstructionSets/subdir.mk
+++ r4300-code-analysis/Debug/InstructionSets/subdir.mk
@@ -23,7 +23,7 @@ C_DEPS += \
 InstructionSets/%.o: ../InstructionSets/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/rjhender/git/R4300-code-analysis/r4300-code-analysis" -I"/home/rjhender/git/R4300-code-analysis/r4300-code-analysis/InstructionSets" -I"/home/rjhender/git/R4300-code-analysis/r4300-code-analysis/Translations" -I"/home/rjhender/git/R4300-code-analysis/r4300-code-analysis/Debugger" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I.. -I"../InstructionSets" -I"../Translations" -I"../Debugger" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
